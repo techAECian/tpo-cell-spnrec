702104bead28825db2afecd21aa471373cda0a0a
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2023-10-02 01:48:05 +0530

    structedFolders

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
-import Home from "./components/Home";
-import About from "./components/About";
-
+import { Home, Contact, About, FAQ, ForCompanies, ForStudents, Terms, NotFound } from "./components/index"
 
 function App() {
 
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="about" element={<About />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="faq" element={<FAQ />} />
+          <Route path="companies" element={<ForCompanies />} />
+          <Route path="students" element={<ForStudents />} />
+          <Route path="terms&condtions" element={<Terms />} />
+          <Route path='*' element={<NotFound />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
 
   )
---
+++ src/components/index.jsx
@@ -0,0 +1,21 @@
+
+import Home from "./home/Home"
+import About from "./about/About"
+import Contact from "./contact/Contact"
+import Terms from "./terms&Contidtions/Terms"
+import ForCompanies from "./forCompanies/ForCompanies"
+import ForStudents from "./forStudents/ForStudents"
+import FAQ from "./FAQ/FAQ"
+import NotFound from "./notFound/NotFound"
+
+export {
+    Home,
+    About,
+    Contact,
+    Terms,
+    ForCompanies,
+    ForStudents,
+    FAQ,
+    NotFound
+
+}
